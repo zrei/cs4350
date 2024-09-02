55d45af01b83e05865436506298c24848a38133b
zrei <88964793+zrei@users.noreply.github.com>
2024-09-02 23:11:06 +0800

    1. Add movement config to global settings 2. Remove occupied squares from pass-through positions

--- Assets/Scripts/Base/GlobalSettings.cs
+++ Assets/Scripts/Base/GlobalSettings.cs
@@ -5,4 +5,8 @@ public class GlobalSettings : Singleton<GlobalSettings>
     [Header("Debug")]
     [SerializeField] private bool m_DoDebug = false;
     public static bool DoDebug => Instance.m_DoDebug;
+
+    [Header("Movement Config")]
+    [SerializeField] private bool m_AllowCrossingOverOccupiedSquares = false;
+    public static bool AllowCrossingOverOccupiedSquares => Instance.m_AllowCrossingOverOccupiedSquares;
 }
