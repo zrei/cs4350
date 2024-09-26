1bfb431020df1e30db6075e994c0c7eba09af2bc
szelongq <72333104+szelongq@users.noreply.github.com>
2024-09-27 01:20:46 +0800

    Fix broken text pointer in asset file
    Add unsubscribing of event listeners onDisable for UnitSetupMenu

--- Assets/Scripts/Base/UI/HUD/UnitSetupMenu.cs
+++ Assets/Scripts/Base/UI/HUD/UnitSetupMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -60,6 +61,11 @@ namespace Game.UI
         private void OnAnimationFinish()
         {
             animator.enabled = false;
+        }
+
+        private void OnDisable()
+        {
+            GlobalEvents.Battle.PlayerUnitSetupStartEvent -= OnSetupStart;
         }
     }
 }
