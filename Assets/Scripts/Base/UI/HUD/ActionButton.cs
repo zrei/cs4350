ca8ab8d2ea7a3c08a7c88d636b34f67390b60ca8
nqt230 <ngqiting@gmail.com>
2024-09-26 13:23:23 +0800

    Set up ActionMenu

---
+++ Assets/Scripts/Base/UI/HUD/ActionButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI
+{
+    public class ActionButton : SelectableBase
+    {
+        public Image border;
+        public Image icon;
+    }
+}
