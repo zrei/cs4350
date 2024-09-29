957ab4ef70f1e987237c5d285d4c63ef0b9b1bc9
szelongq <72333104+szelongq@users.noreply.github.com>
2024-09-29 21:37:24 +0800

    Add chest token for reward nodes

--- Assets/Scripts/Level/Nodes/NodeVisuals/RewardNodeVisual.cs
+++ Assets/Scripts/Level/Nodes/NodeVisuals/RewardNodeVisual.cs
@@ -8,6 +8,9 @@ public class RewardNodeVisual : NodeVisual
 {
     private RewardNode m_RewardNode;
     
+    // Chest token model
+    [SerializeField] private GameObject m_chestToken;
+    
     public override void Initialise()
     {
         m_RewardNode = GetComponent<RewardNode>();
@@ -19,10 +22,12 @@ public class RewardNodeVisual : NodeVisual
         if (m_RewardNode.IsCurrent)
         {
             SetNodeState(NodePuckType.CURRENT);
+            m_chestToken.SetActive(false);
         }
         else
         {
             SetNodeState(m_RewardNode.IsCleared ? NodePuckType.CLEARED : NodePuckType.REWARD);
+            m_chestToken.SetActive(!m_RewardNode.IsCleared);
         
             if (m_RewardNode.IsGoalNode)
             {
