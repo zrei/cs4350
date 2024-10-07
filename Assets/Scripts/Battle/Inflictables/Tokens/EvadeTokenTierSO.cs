897e1cfcc5781f75bc3e0893705d0134cff6657d
zrei <88964793+zrei@users.noreply.github.com>
2024-10-07 23:37:30 +0800

    Fix evade token type not being set

--- Assets/Scripts/Battle/Inflictables/Tokens/EvadeTokenTierSO.cs
+++ Assets/Scripts/Battle/Inflictables/Tokens/EvadeTokenTierSO.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EvadeTokenTierSO", menuName = "ScriptableObject/Inflictables/Token/EvadeTokenTierSO")]
 public class EvadeTokenTierSO : TokenTierSO
 {
-
+    public override TokenType TokenType => TokenType.EVADE;
 }
