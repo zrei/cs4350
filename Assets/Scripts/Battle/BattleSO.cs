e3901bf2a053959f7552d76b28aff2e4c97764a9
zrei <88964793+zrei@users.noreply.github.com>
2024-10-12 12:10:31 +0800

    SO formatting

--- Assets/Scripts/Battle/BattleSO.cs
+++ Assets/Scripts/Battle/BattleSO.cs
@@ -29,12 +29,14 @@ public class BattleSO : ScriptableObject
     [Header("Win Conditions")]
     // only one win condition is allowed
     public WinCondition m_WinCondition = WinCondition.DEFEAT_REQUIRED;
+    [Space]
     [Tooltip("Number of turns the player needs to survive - this is ignored if the win condition is not SURVIVE_TURNS")]
     public float m_TurnsToSurvive = 25f;
 
     [Header("Lose conditions")]
     [Tooltip("Secondary lose conditions apart from certain units being defeated")]
     public SecondaryLoseCondition[] m_AdditionalLoseConditions;
+    [Space]
     [Tooltip("Maximum number of turns the player is given to complete the battle - this is ignored if TOO_MANY_TURNS is not one of the secondary lose conditions")]
     // note: this should not be mixed with a win condition of SURVIVE_TURNS.
     public float m_MaxTurns = 25f;
