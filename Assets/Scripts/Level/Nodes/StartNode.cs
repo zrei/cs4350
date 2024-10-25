8cbb358efa36dd55107dc3ba88d99352007b9210
szelongq <72333104+szelongq@users.noreply.github.com>
2024-10-25 11:30:51 +0800

    Add dialogue to start node

--- Assets/Scripts/Level/Nodes/StartNode.cs
+++ Assets/Scripts/Level/Nodes/StartNode.cs
@@ -1,7 +1,40 @@
+using Game.UI;
 using UnityEngine;
 
 public class StartNode : NodeInternal
 {
+    [SerializeField] private Dialogue m_DefaultDialogue;
+    [SerializeField] private ConditionalDialogue[] m_ConditionalDialogues;
+    
+    public void StartNodeEvent(VoidEvent onEventFinished)
+    {
+        foreach (var conditionalDialogue in m_ConditionalDialogues)
+        {
+            if (conditionalDialogue.IsConditionsSatisfied())
+            {
+                DialogueDisplay.Instance.StartDialogue(conditionalDialogue.m_Dialogue);
+                GlobalEvents.Dialogue.DialogueEndEvent += onDialogueEnd;
+                return;
+            }
+        }
+
+        if (m_DefaultDialogue != null)
+        {
+            DialogueDisplay.Instance.StartDialogue(m_DefaultDialogue);
+            GlobalEvents.Dialogue.DialogueEndEvent += onDialogueEnd;
+            return;
+        }
+        
+        onEventFinished?.Invoke();
+        return;
+
+        void onDialogueEnd()
+        {
+            GlobalEvents.Dialogue.DialogueEndEvent -= onDialogueEnd;
+            onEventFinished?.Invoke();
+        }
+    }
+    
     public override void Initialise()
     {
         // Cleared by default
