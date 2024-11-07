a23945f0ca62df2a68125e2087d3d04012e607bd
zrei <88964793+zrei@users.noreply.github.com>
2024-11-07 21:12:28 +0800

    Revamp cutscene system

--- Assets/Scripts/Cutscene/Cutscene.cs
+++ Assets/Scripts/Cutscene/Cutscene.cs
@@ -1,20 +1,16 @@
-using Cinemachine;
 using UnityEngine;
 
 public class Cutscene : MonoBehaviour
 {
-    public GameObject m_CutsceneObj;
-    public Transform m_CutsceneTransform;
-    public Transform m_VCamTransform;
+    [SerializeField] private GameObject m_CutsceneObj;
+    [SerializeField] private Transform m_CutsceneTransform;
     public Dialogue m_Dialogue;
 
     private GameObject m_CutsceneInstance = null;
 
-    public void InstantiateCutscene(CinemachineVirtualCamera virtualCamera)
+    public void InstantiateCutscene()
     {
         m_CutsceneInstance = Instantiate(m_CutsceneObj, m_CutsceneTransform.position, m_CutsceneTransform.rotation, m_CutsceneTransform);
-        virtualCamera.transform.position = m_VCamTransform.position;
-        virtualCamera.transform.rotation = m_VCamTransform.rotation;
     }
 
     public void EndCutscene()
--- Assets/Scripts/WorldMap/WorldMapCutsceneManager.cs
+++ Assets/Scripts/WorldMap/WorldMapCutsceneManager.cs
@@ -1,11 +1,8 @@
-using Cinemachine;
 using Game.UI;
 using UnityEngine;
 
 public class WorldMapCutsceneManager : MonoBehaviour
-{
-    [SerializeField] CinemachineVirtualCamera m_CutsceneVCam;
-    
+{    
     private Cutscene m_CurrCutscene = null;
     private VoidEvent m_PostCutsceneCallback = null;
     private int m_InitialCullingMask = -1;
@@ -13,23 +10,35 @@ public class WorldMapCutsceneManager : MonoBehaviour
     public void ShowCutscene(Cutscene cutscene, VoidEvent postCutscene)
     {
         m_CurrCutscene = cutscene;
-        m_CurrCutscene.InstantiateCutscene(m_CutsceneVCam);
+        m_CurrCutscene.InstantiateCutscene();
 
         m_InitialCullingMask = Camera.main.cullingMask;
         Camera.main.cullingMask = ~LayerMask.GetMask("WorldMap");
 
-        m_CutsceneVCam.enabled = true;
         m_PostCutsceneCallback = postCutscene;
 
         GlobalEvents.Dialogue.DialogueEndEvent += EndCutscene;
-        DialogueDisplay.Instance.StartDialogue(cutscene.m_Dialogue);
+
+        /*if (!DialogueDisplay.IsReady)
+        {
+            DialogueDisplay.OnReady += PostDependency;
+        }
+        else
+        {
+            PostDependency();
+        }
+
+        void PostDependency()
+        {
+            DialogueDisplay.OnReady -= PostDependency;*/
+            DialogueDisplay.Instance.StartDialogue(cutscene.m_Dialogue);
+        //}
     }
 
     private void EndCutscene()
     {
         GlobalEvents.Dialogue.DialogueEndEvent -= EndCutscene;
 
-        m_CutsceneVCam.enabled = false;
         Camera.main.cullingMask = m_InitialCullingMask;
         
         m_CurrCutscene.EndCutscene();
