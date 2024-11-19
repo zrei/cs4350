c731db7ecde762aafab4bd2fe923e128d8e39949
nqt230 <ngqiting@gmail.com>
2024-11-19 21:01:36 +0800

    Fix color adjustment for skill buttons

--- Assets/Scripts/Base/UI/HUD/ActionMenu/SkillButton.cs
+++ Assets/Scripts/Base/UI/HUD/ActionMenu/SkillButton.cs
@@ -13,31 +13,32 @@ namespace Game.UI
     public class SkillButton : ActionButton
     {
         public Color defaultIconColor = Color.red;
+        public Image softGlow;
+        public Image darken;
         public Image fillImage;
         public FormattedTextDisplay statusText;
-        public Image softGlow;
-        public GraphicGroup graphicGroup;
 
-        public Sprite SkillSprite {
-            set {
-                fillImage.sprite = value;
+        public Sprite SkillSprite
+        {
+            set
+            {
                 icon.sprite = value;
             }
         }
 
         public void SetFill(float fill)
         {
-            fillImage.fillAmount = fill;
-            softGlow.gameObject.SetActive(fill >= 1f);
-            graphicGroup.color = fill >= 1f ? defaultIconColor : Color.grey;
+            Debug.Log(fill);
+            fillImage.fillAmount = 1f - fill;
         }
 
         public void SetState(SkillButtonState skillButtonState)
         {
             interactable = skillButtonState != SkillButtonState.EMPTY;
-            icon.color = skillButtonState == SkillButtonState.EMPTY ? Color.clear : Color.gray;
-            fillImage.color = skillButtonState == SkillButtonState.EMPTY ? Color.clear : defaultIconColor;
+            icon.gameObject.SetActive(skillButtonState != SkillButtonState.EMPTY);
+            darken.gameObject.SetActive(skillButtonState == SkillButtonState.LOCKED);
             statusText.gameObject.SetActive(skillButtonState == SkillButtonState.LOCKED);
+            softGlow.gameObject.SetActive(skillButtonState == SkillButtonState.NORMAL);
         }
     }
 }
