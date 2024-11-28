c4b38a32358054d112d92bad7c84fc23d828afe3
zrei <88964793+zrei@users.noreply.github.com>
2024-11-28 20:59:00 +0800

    Add tile effect SO

---
+++ Assets/Scripts/Battle/Grid/TileEffectSO.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public enum TileEffectType 
+{
+
+}
+
+public class TileEffectSO : ScriptableObject
+{
+    public TileEffect GetTileEffect()
+    {
+        return null;
+    }
+}
+
+public class TileEffect 
+{
+
+}
