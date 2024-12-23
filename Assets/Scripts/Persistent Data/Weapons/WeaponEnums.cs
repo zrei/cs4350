d0eb8ba45a98cbd4e8e9e07751a4bd72592be5da
nqt230 <ngqiting@gmail.com>
2024-12-23 22:33:27 +0800

    Integrate new animations to base anim controller

--- Assets/Scripts/Persistent Data/Weapons/WeaponEnums.cs
+++ Assets/Scripts/Persistent Data/Weapons/WeaponEnums.cs
@@ -12,5 +12,9 @@ public enum WeaponAnimationType
     STAFF = 100,
     SWORD = 200,
     HEAVY_SWORD = 300,
-    BOW = 400
+    BOW = 400,
+    SPEAR = 500,
+    DUAL = 600,
+    MOUNT = 700,
+    DASH = 1000,
 }
